f97281dbb6d13c1faff5e04c37ddb2a88d1039db
nachiketkallapur <nachiketkallapur@gmail.com>
2020-12-05 00:51:39 +0530

    Connected with mysql

--- routes/player.js
+++ routes/player.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var sql = require('../mysql-connection');
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
@@ -10,5 +11,14 @@ router.post('/',(req,res,next) => {
  console.log("Player Body received ",req.body);
   res.send("Successfully player body received").status(200);
 }) 
+
+router.get('/test', (req,res,next) => {
+  sql.query(`SELECT * FROM people`,(err,results,fields) => {
+    if(err) console.log(error);
+    else res.send(results);
+  })
+
+  // res.send("Success").status(200);
+})
 
 module.exports = router;
