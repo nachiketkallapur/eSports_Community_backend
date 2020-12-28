2b364766f2994464649cc3bb63c082418725b8a7
nachiketkallapur <nachiketkallapur@gmail.com>
2020-12-29 01:04:36 +0530

    added clan/addNewPlayer post route and modified clan/update route to add clan leader to clan if he is not present previously

--- routes/clan.js
+++ routes/clan.js
@@ -145,6 +145,7 @@ router.post("/update", (req, res, next) => {
     G_name,
     C_email,
     P_username,
+    C_level,
   } = req.body;
   var isResponseSent = false;
   var doesPlayerExist;
@@ -162,19 +163,22 @@ router.post("/update", (req, res, next) => {
             doesPlayerExist = false;
             return;
           }
-        } else if(results.length===0){
-
+        } else if (results.length === 0) {
           console.log("Player doesn't exist");
-          if(!isResponseSent){
-            res.send({message: "Invalid clan leader. Such player doesn't exist",error:true});
-            isResponseSent=true;
+          if (!isResponseSent) {
+            res.send(
+              {
+                message: "Invalid clan leader. Such player doesn't exist",
+                error: true,
+              },
+            );
+            isResponseSent = true;
             doesPlayerExist = false;
             return;
           }
-
         } else {
-          console.log('Valid clan leader');
-          doesPlayerExist=true;
+          console.log("Valid clan leader");
+          doesPlayerExist = true;
         }
       },
     );
@@ -183,17 +187,81 @@ router.post("/update", (req, res, next) => {
     if (!isResponseSent) {
       res.send({ message: err.message, error: true });
       isResponseSent = true;
-      doesPlayerExist=false;
+      doesPlayerExist = false;
+      return;
+    }
+  }
+
+  var isLeaderAlreadyMemberOfClan;
+
+  try{
+
+    sql.query('select * from player_isMemberOf_clan where P_username=?and C_name=? ',
+    [P_username,C_name],
+    (err,results,fields)=>{
+      if(err){
+        console.log(err);
+        if(!isResponseSent){
+          res.send({error:true,message:err.sqlMessage});
+          isResponseSent=true;
+          return;
+        }
+
+      } else if(results.length===0){
+        isLeaderAlreadyMemberOfClan=false;
+      } else {
+        isLeaderAlreadyMemberOfClan=true;
+      }
+    })
+
+  } catch(err){
+    console.log(err);
+    if(!isResponseSent){
+      res.send({error:true,message:err.message});
+      isResponseSent=true;
       return;
     }
   }
 
   setTimeout(() => {
     if (doesPlayerExist === true) {
+
+      if(isLeaderAlreadyMemberOfClan===false){
+        try{
+
+          sql.query("insert into player_isMemberOf_clan(P_username,C_name,activity) values?",
+          [[[P_username,C_name,0]]],
+          (err,results,fields)=>{
+            if(err){
+              console.log(err);
+              if(!isResponseSent){
+                res.send({error:true, message:err.sqlMessage});
+                isResponseSent=true;
+                return;
+              }
+            
+            }else {
+              console.log("Leader inserted into the clan")
+            }
+
+          })
+
+        } catch(err){
+          console.log(err);
+          if(!isResponseSent){
+            res.send({error:true,message:err.message});
+            isResponseSent=true;
+            return;
+          }
+        }
+      }
+
+
+
       try {
         sql.query(
-          "update clan set C_category=?, C_size=?, G_name=?,C_email=?,P_username=? where C_name=?",
-          [C_category, C_size, G_name, C_email, P_username, C_name],
+          "update clan set C_category=?, C_size=?, G_name=?,C_email=?,P_username=?,C_level=? where C_name=?",
+          [C_category, C_size, G_name, C_email, P_username, C_level, C_name],
           (err, results, fields) => {
             if (err) {
               console.log(err.sqlMessage);
@@ -226,4 +294,122 @@ router.post("/update", (req, res, next) => {
   }, 1000);
 });
 
+router.post("/addNewPlayer", (req, res, next) => {
+  /*First time player joins any clan*/
+  var isResponseSent = false;
+
+  const { playerUsername, clanUsername } = req.body;
+  const activity = 0;
+  var clanName;
+
+  try{
+
+    sql.query('select * from player where P_username=?',[[playerUsername]],
+    (err,results,fields)=>{
+      if(err){
+
+        console.log(err);
+        if(!isResponseSent){
+          res.send({error:true, message:err.sqlMessage});
+          isResponseSent=true;
+          return;
+        }
+
+      }else if(results.length===0){
+        console.log("Such player doesn't exist");
+        if(!isResponseSent){
+          res.send({error:true, message:"Such player doesn't exist"});
+          isResponseSent=true;
+          return;
+        }
+
+      } else {
+        console.log("Player exists in DB")
+      }
+
+    })
+
+  }catch(err){
+
+    console.log(err);
+    if(!isResponseSent){
+      res.send({error:true,message:err.message});
+      isResponseSent=true;
+      return;
+    }
+  }
+
+
+  try {
+    sql.query(
+      "select C_name from clan where C_username=?",
+      [[clanUsername]],
+      (err, results, fields) => {
+        if (err) {
+          console.log(err);
+          if (!isResponseSent) {
+            res.send({ error: true, message: err.sqlMessage });
+            return;
+          }
+        } else if (results.length === 0) {
+          console.log("Such clan doesn't exist");
+          if (!isResponseSent) {
+            res.send({ error: true, message: "Such clan doesn't exist" });
+            isResponseSent = true;
+            return;
+          }
+        } else {
+          console.log("Fetched clanName from clanUsername");
+          clanName = results[0].C_name;
+        }
+      },
+    );
+  } catch (err) {
+    console.log(err);
+    if (!isResponseSent) {
+      res.send({ error: true, message: err.message });
+      isResponseSent = true;
+      return;
+    }
+  }
+
+  setTimeout(() => {
+    try {
+      sql.query(
+        "insert into player_isMemberOf_clan(P_username,C_name,activity) values?",
+        [[[playerUsername, clanName, activity]]],
+        (err, results, fields) => {
+          if (err) {
+            console.log(err);
+            if (!isResponseSent) {
+              res.send({ error: true, message: err.sqlMessage });
+              isResponseSent = true;
+              return;
+            }
+          } else {
+            console.log("Player added to clan successfully");
+            if (!isResponseSent) {
+              res.send(
+                {
+                  error: false,
+                  message:
+                    `${playerUsername} added to ${clanName} successfully`,
+                },
+              );
+              isResponseSent = true;
+              return;
+            }
+          }
+        },
+      );
+    } catch (err) {
+      console.log(err);
+      if (!isResponseSent) {
+        res.send({ error: true, message: err.message });
+        isResponseSent = true;
+        return;
+      }
+    }
+  }, 1000);
+});
 module.exports = router;
